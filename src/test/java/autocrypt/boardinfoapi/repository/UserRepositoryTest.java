4d28eca0484149ad45aac82fb043030ebe844163
sungjin <sungjin0757@naver.com>
2022-06-27 02:36:08 +0900

    feat(common) : MySql Setting

--- src/test/java/autocrypt/boardinfoapi/repository/UserRepositoryTest.java
+++ src/test/java/autocrypt/boardinfoapi/repository/UserRepositoryTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 @Transactional
 class UserRepositoryTest {
     @Autowired
